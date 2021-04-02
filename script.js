e491d9c8501e81803f17257f5cc89e5c8040d387
marleigh <volcana@gmail.com>
2021-04-01 20:02:54 -0700

    MATH WHY

--- script.js
+++ script.js
@@ -20,8 +20,22 @@ let offset = Math.floor((Math.random() * (max - min + 1)) + min); //need to figu
 // let modal, clueDivs;
 // let clueValues = [];
 
-let content = [];
-let clueObj =[];
+let content = [],
+    clueObj = [],
+    testArray = [],
+    finalArr = [];
+
+function ClueObject (question, answer, id, value, airdate) {
+    this.question = question;
+    this.answer = answer;
+    this.id = id;
+    this.value = value;
+    this.airdate = airdate;
+}
+
+function findValue(obj, val) {
+    return obj.value === val;
+}
 
 const grid = document.querySelector(".grid");
 const container = document.querySelector(".container");
@@ -35,68 +49,69 @@ function getCategories() {
                 let arr = [data.id, data.title];
                 content.push(arr);
                 
+                
                 let div = document.createElement('div');
                 div.classList.add('grid-item', 'category');
                 div.textContent = data.title;
                 grid.appendChild(div);
             });
         })
-        .then(data => {
-            //we're not actually using 'data' oops
+        .then(() => {
             content.forEach(category => {
                 let id = category[0];
-
                 fetch(`https://jservice.io/api/clues/?category=${category[0]}`) //i believe this is implying that its actually category[foreachposition][0]
                     .then(response => response.json())
                     .then(data => {
-                        console.log(data);
-                        /*
-                            PROBLEMS:
-                            pulling in way more numbers than we need - might be related to the issues below
-                            we got some categories with more questions/answers than we need
-                            some are OUT OF ORDER. starting with 200
-                            need to sort by value. 
-                            THEN we need to only grab 6 - one FOR EACH lol value we need
 
-                        */
+                        //this will gather all the clues for each category and push them into an array
+                        data.forEach(clue => {                            
+                            let newObj = new ClueObject(clue.question, clue.answer, clue.category_id, clue.value, clue.airdate);
+                            testArray.push(newObj);
+                            // {
+                            //     "question": clue.question,
+                            //     "answer": clue.answer,
+                            //     "airDate": clue.airdate,
+                            //     "value": clue.value,
+                            // }
 
-                        data.forEach(clue => {
-                            clueObj.push(
-                                {
-                                    "question": clue.question,
-                                    "answer": clue.answer,
-                                    "airDate": clue.airdate,
-                                    "value": clue.value
-                                }
-                            );
-                            let div = document.createElement('div');
-                            div.classList.add('grid-item', 'clue');
-                            div.setAttribute("id", `${id}-hi`); //need to make this unique based on the position in the grid
-                            //but need to actually set the grid up properly and WHY ARE THINGS NULL
-                            div.textContent = `$${clue.value}`;
-                            //console.log(div)
-                            grid.appendChild(div);
+                            // let div = document.createElement('div');
+                            // div.classList.add('grid-item', 'clue');
+                            // div.setAttribute("id", `${id}-hi`); //need to make this unique based on the position in the grid
+                            // //but need to actually set the grid up properly and WHY ARE THINGS NULL
+                            // div.textContent = `$${clue.value}`;
+                            // grid.appendChild(div);
                         });
 
-                    })
+
+                        //filters all the clues  of the current category into an array
+                        let categoryArr = testArray.filter(obj => obj.id == id);
+
+                        //check to see if the length of said array is greater than 5, in which case it needs to be checked out
+                        if(categoryArr.length > 5) {
+
+                            //for as long as the number is less than the array's length
+                            for(let i = 0; i < categoryArr.length; i++) {
+                                let valueCheck = 200;
+                                //find the first one that match the above yes?? 
+                                finalArr.push(categoryArr.find(cat => cat.value === valueCheck));
+                                console.log(finalArr);
+                         }
+
+                        }
+
+                    });
 
             });
 
-        })
+        });
 
     // return await result.json();
-    testFunction(clueObj)
+
 }
 
 getCategories();
 
-function testFunction(obj) {
-    console.log(obj)
-    obj.forEach(obj => {
 
-        console.log(clueObj.value);
-    })
-}
 // getCategories().then(json => {
 //     json.forEach(category => {
 //         let div = document.createElement('div');
